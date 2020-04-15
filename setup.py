4c5f7471fa3e1567a0179f1ac77d3a6abce8ba2d
Sara Zanzottera <sara.zanzottera@cern.ch>
2020-04-15 15:39:53 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ REQUIREMENTS: dict = {
 
 setup(
     name='be-bi-pyqt-project-manager',
-    version="0.0.1.dev1",
+    version="0.0.1.dev2",
 
     author="Sara Zanzottera",
     author_email="sara.zanzottera@cern.ch",
