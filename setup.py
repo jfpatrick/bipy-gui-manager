f5e499b9ed40801a8033e4c416b9b8907dcdfb5d
Sara Zanzottera <sara.zanzottera@cern.ch>
2020-05-05 13:59:23 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ REQUIREMENTS: dict = {
 
 setup(
     name='bipy_gui_manager',
-    version="0.1.0",
+    version="0.2.0",
 
     author="Sara Zanzottera",
     author_email="sara.zanzottera@cern.ch",
