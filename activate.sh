eaf2ed847047d67985efa62c50a7a270822e47a4
Sara Zanzottera <sara.zanzottera@cern.ch>
2021-03-08 14:18:59 +0000

    Update activate.sh

--- activate.sh
+++ activate.sh
@@ -1,7 +1,4 @@
 # Activate the virtual environments
 deactivate >/dev/null 2>&1
-source /acc/local/share/python/acc-py-pyqt/setup.sh
+source /acc/local/share/python/acc-py/base/pro/setup.sh
 source venv/bin/activate
-
-# Put CO's PyCharm on the PATH
-export PATH=$PATH:/acc/local/share/python/pycharm/pycharm-community-2019.2.3/bin
