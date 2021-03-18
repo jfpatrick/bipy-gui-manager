1710ae9c067fbf1df51ea1189fbc3c9579e29b3f
Sara Zanzottera <sara.zanzottera@cern.ch>
2021-03-18 09:32:03 +0000

    Update app_deploy.sh

--- bipy_gui_manager/deploy/resources/app_deploy.sh
+++ bipy_gui_manager/deploy/resources/app_deploy.sh
@@ -14,14 +14,19 @@ if [ $VERBOSE = "1" ]; then
 
     # Deploy to shared folder
     echo -e "-> Deploying application (can take a few minutes)..."
-    if ! $ACC_PY_PATH/acc-py app deploy --deploy-base $REPO_PATH $APP_PATH; then
-        echo -e "   Deployment failed! Please try running 'acc-py app deploy --deploy-base $REPO_PATH $APP_PATH' and check the logs."
-        echo -e "   If you are deploying a new version, make sure you increased the version number (re-deploy is not allowed)"
+    if ! $ACC_PY_PATH/acc-py app lock $APP_PATH; then
+        echo -e "   Dependency lock failed! Please try running 'acc-py app lock $APP_PATH' and check the logs."
         exit 1
     else
-        echo -e "   Deployment successful. You can now run your application with bipy-gui-manager run $(basename $APP_PATH)."
-        exit 0
+        if ! $ACC_PY_PATH/acc-py app deploy --deploy-base $REPO_PATH $APP_PATH; then
+            echo -e "   Deployment failed! Please try running 'acc-py app deploy --deploy-base $REPO_PATH $APP_PATH' and check the logs."
+            echo -e "   If you are deploying a new version, make sure you increased the version number (re-deploy is not allowed)"
+            exit 1
+        else
+            echo -e "   Deployment successful. You can now run your application with bipy-gui-manager run $(basename $APP_PATH)."
+            exit 0
 
+        fi
     fi
 
 else
@@ -31,13 +36,18 @@ else
 
     # Deploy to shared folder
     echo -e "-> Deploying application (can take a few minutes)..."
-    if ! $ACC_PY_PATH/acc-py app deploy --deploy-base $REPO_PATH $APP_PATH &> /dev/null; then
-        echo -e "   Deployment failed! Please try running 'acc-py app deploy --deploy-base $REPO_PATH $APP_PATH' and check the logs."
-        echo -e "   If you are deploying a new version, make sure you increased the version number (re-deploy is not allowed)"
+    if ! $ACC_PY_PATH/acc-py app lock $APP_PATH; then
+        echo -e "   Dependency lock failed! Please try running 'acc-py app lock $APP_PATH' and check the logs."
         exit 1
     else
-        echo -e "   Deployment successful. You can now run your application with bipy-gui-manager run $(basename $APP_PATH)."
-        exit 0
+        if ! $ACC_PY_PATH/acc-py app deploy --deploy-base $REPO_PATH $APP_PATH; then
+            echo -e "   Deployment failed! Please try running 'acc-py app deploy --deploy-base $REPO_PATH $APP_PATH' and check the logs."
+            echo -e "   If you are deploying a new version, make sure you increased the version number (re-deploy is not allowed)"
+            exit 1
+        else
+            echo -e "   Deployment successful. You can now run your application with bipy-gui-manager run $(basename $APP_PATH)."
+            exit 0
 
+        fi
     fi
 fi
